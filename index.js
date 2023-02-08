56bc70d457610a238993e7e0c905c9cb4419811e
eudematos <eudematos@sudamericano.edu.ec>
2023-02-08 10:53:35 -0500

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let doc = document.getElementById("img01")
 let url = prompt ('Ingrse id Url gdrive');
-doc.src=`https://drive.google.com/uc?export=view&id=${1kQcAHVoesQCJyJlZ3Zef_0t-UgIfkkhz}`;
+doc.src=`https://drive.google.com/uc?export=view&id=${1TURr7ncn3V1a1hRwzIyT7vMv5BdABfUY}`;
